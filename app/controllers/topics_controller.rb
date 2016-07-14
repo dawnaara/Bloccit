87b39a8dd4334954a51fe9cd1e94ad922eb7977f
Dawna Ara <dawnaara@gmail.com>
2016-07-14 10:08:51 -0700

    assignment 40 attempted

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -31,18 +31,18 @@ class TopicsController < ApplicationController
      end
    end
 
-      def update
-     @topic = Topic.find(params[:id])
-     @topic.assign_attributes(topic_params)
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.assign_attributes(topic_params)
  
-     if @topic.save
-        flash[:notice] = "Topic was updated successfully."
-       redirect_to @topic
-     else
-       flash.now[:alert] = "Error saving topic. Please try again."
-       render :edit
-     end
-   end
+    if @topic.save
+      flash[:notice] = "Topic was updated successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
 
 
   def destroy
@@ -59,13 +59,14 @@ class TopicsController < ApplicationController
 
       private
  
-   def topic_params
-     params.require(:topic).permit(:name, :description, :public)
-   end
-      def authorize_user
-     unless current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
-       redirect_to topics_path
-     end
-   end
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
+
+  def authorize_user
+    unless current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
 end
