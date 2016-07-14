029ba0422ad9b27eb0746ca5f43ea342e2172988
Dawna Ara <dawnaara@gmail.com>
2016-07-13 22:38:31 -0700

    checkpoint 40 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713234246) do
+ActiveRecord::Schema.define(version: 20160714050255) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160713234246) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
