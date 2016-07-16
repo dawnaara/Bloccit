0459b20733a0a3348534225639c1d51dbb7e4df6
Dawna Ara <dawnaara@gmail.com>
2016-07-15 18:12:05 -0700

    checkpoint 41 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,12 @@ class UsersController < ApplicationController
    	end
 
   def create
-<<<<<<< HEAD
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
  
-
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
       create_session(@user)
@@ -22,20 +20,4 @@ class UsersController < ApplicationController
       render :new
     end
   end
-=======
-     @user = User.new
-     @user.name = params[:user][:name]
-     @user.email = params[:user][:email]
-     @user.password = params[:user][:password]
-     @user.password_confirmation = params[:user][:password_confirmation]
- 
-     if @user.save
-       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-       create_session(@user)
-       redirect_to root_path
-     else
-       flash.now[:alert] = "There was an error creating your account. Please try again."
-       render :new
-     end
-   end
 end
