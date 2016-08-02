4ba0792dae160ce7521872d7f311623c11b76828
Dawna Ara <dawnaara@gmail.com>
2016-08-02 13:52:37 -0700

    assignment 48 completed

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -3,6 +3,7 @@
  RSpec.describe Api::V1::TopicsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
  
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -14,7 +15,13 @@
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
      end
-   end
+
+     it "GET show returns child posts" do
+      get :show, id: my_topic.id 
+      response_hash = JSON.parse response.body
+      expect(response).to have_http_status(:success)
+     end
+   end 
  
    context "unauthorized user" do
      before do
@@ -30,5 +37,11 @@
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
      end
+
+     it "GET show returns child posts" do
+      get :show, id: my_topic.id 
+      response_hash = JSON.parse response.body
+      expect(response).to have_http_status(:success)
+     end     
    end
  end
